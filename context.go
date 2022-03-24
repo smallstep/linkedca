6a170f8e86415ae5a950a265ad22ee71dfc51cad
Herman Slatman <hermanslatman@hotmail.com>
2022-03-24 17:41:59 +0100

    Add methods for adding/getting provisioner to/from ctx

--- context.go
+++ context.go
@@ -2,16 +2,34 @@ package linkedca
 
 import "context"
 
-type contextKey struct{}
+type contextKeyType int
 
-// WithAdmin returns a copy of ctx which carries admin.
-func WithAdmin(ctx context.Context, admin *Admin) context.Context {
-	return context.WithValue(ctx, contextKey{}, admin)
+const (
+	_ contextKeyType = iota
+	adminContextKey
+	provisionerContextKey
+)
+
+// NewContextWithAdmin returns a copy of ctx which carries an Admin.
+func NewContextWithAdmin(ctx context.Context, admin *Admin) context.Context {
+	return context.WithValue(ctx, adminContextKey, admin)
 }
 
 // AdminFromContext returns the Admin ctx carries.
 //
 // AdminFromContext panics in case ctx carries no Admin.
 func AdminFromContext(ctx context.Context) *Admin {
-	return ctx.Value(contextKey{}).(*Admin)
+	return ctx.Value(adminContextKey).(*Admin)
+}
+
+// NewContextWithProvisioner returns a copy of ctx which carries a Provisioner.
+func NewContextWithProvisioner(ctx context.Context, provisioner *Provisioner) context.Context {
+	return context.WithValue(ctx, provisionerContextKey, provisioner)
+}
+
+// ProvisionerFromContext returns the Provisioner ctx carries.
+//
+// ProvisionerFromContext panics in case ctx carries no Provisioner.
+func ProvisionerFromContext(ctx context.Context) *Provisioner {
+	return ctx.Value(provisionerContextKey).(*Provisioner)
 }
